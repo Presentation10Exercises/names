fa6b4ff89ec1cdaf4b3703bbaca9d0d407dd8b50
My Name <imakestuff@420blaze.it>
2015-12-18 04:48:39 -0800

    Initial commit

---
+++ foo.go
@@ -0,0 +1,15 @@
+package main
+
+
+import ("fmt")
+
+func main() {
+
+  var myvar string
+  myvar = "Kai"
+  fmt.Println("My name is What?")
+  fmt.Println("My name is Who?")
+  fmt.Println("My name is " + myvar)
+
+
+}
