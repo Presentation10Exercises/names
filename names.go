045b1d6e5eaff6a627d4d8ac327a51a27b510a35
My Name <imakestuff@420blaze.it>
2015-12-18 18:12:49 -0800

    removed variable declaration

--- names.go
+++ names.go
@@ -2,13 +2,12 @@ package names
 
 import "fmt"
 
-var MyName string = "Bartholomew"
-
 func init() {
   MyName := "something"
   fmt.Println(MyName)
 }
 
 func GetName() string {
+  MyName := "something else"
   return MyName
 }
